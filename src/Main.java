a1455b15cbdaa242cd310f845cee66a29ba01d3c
Valerii <lord_liret@mail.ru>
2023-07-11 08:24:50 +0500

    домашнее задание 2 задача 1

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        task13();
+        task45();
+        task68();
+        taskTest();
+    }
+
+    public static void task13 () {
+        System.out.println("Задача 1");
+        var dog =8.0;
+        var cat =3.6;
+        var paper=763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+
+    public static void task45 () {
+        System.out.println("Задача 4-5");
+        // Пишем код для задачи 2
+    }
+
+    public static void task68 () {
+        System.out.println("Задача 6-8");
+        // Пишем код для задачи 1
+    }
+
+    public static void taskTest () {
+        System.out.println("Задача test");
+        // Пишем код для задачи 1
+    }
+}
